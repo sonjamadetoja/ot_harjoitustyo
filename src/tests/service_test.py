32fb413b25efed266e7423ee53ba9f8151848407
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2021-11-30 14:41:40 +0200

    fix tests

--- src/tests/service_test.py
+++ src/tests/service_test.py
@@ -1,24 +1,25 @@
 import unittest
-from services.service import Service
+from services.service import service
 
-class TestService:
+class TestService(unittest.TestCase):
     def setUp(self):
-        self.service = Service()
+        pass
 
     def test_login_with_valid_username(self):
-        response = self.service.login("Testaaja")
+        response = service.login("Testaaja")
 
-        self.AssertEqual(response, "login")
+        self.assertEqual(response, "login")
 
     def test_login_with_invalid_username(self):
-        response = self.service.login("Testaja")
+        response = service.login("Testaja")
 
-        self.AssertEqual(response, None)
+        self.assertEqual(response, None)
 
     # def test_register_creates_a_new_username(self):
     #     pass
+    #    self.assertEqual(response, "register")
 
     def test_register_does_not_create_if_username_exists(self):
-        response = self.service.register("Testaaja")
+        response = service.register("Testaaja")
 
-        self.AssertEqual(response, False)
+        self.assertEqual(response, False)
