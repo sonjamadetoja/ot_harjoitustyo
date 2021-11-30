791d073ad54c4913b694d460cfbf42bee37b5774
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2021-11-30 15:12:09 +0200

    add coverage

--- tasks.py
+++ tasks.py
@@ -11,3 +11,11 @@ def lint(ctx):
 @task
 def test(ctx):
     ctx.run("pytest src")
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src")
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html")
