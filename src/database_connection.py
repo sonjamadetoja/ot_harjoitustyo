fc16331af5ce785e69e430f61419b5d8468a48f1
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2021-12-07 20:38:45 +0200

    changes for testing

--- src/database_connection.py
+++ src/database_connection.py
@@ -8,3 +8,8 @@ connection.row_factory = sqlite3.Row
 
 def get_database_connection():
     return connection
+
+def get_database_connection_test():
+    connection_test = sqlite3.connect(os.path.join(dirname, '..', 'data', "database_test.sqlite"))
+    connection_test.row_factory = sqlite3.Row
+    return connection_test
--- src/initialize_database.py
+++ src/initialize_database.py
@@ -2,28 +2,52 @@ from repositories.user_repository import user_repository
 
 class DataBase:
 
-    def __init__(self, connection):
+    def __init__(self, connection, user_repository):
         self._connection = connection
+        self._user_repository = user_repository
 
     def create_tables(self):
         cursor = self._connection.cursor()
-
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT UNIQUE);
             ''')
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS transactions (id INTEGER PRIMARY KEY, date TIMESTAMP, deposits INTEGER, user_id INTEGER REFERENCES users, title TEXT, category TEXT);
             ''')
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS test_users (id INTEGER PRIMARY KEY, username TEXT UNIQUE);
+            ''')
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS test_transactions (id INTEGER PRIMARY KEY, date TIMESTAMP, deposits INTEGER, user_id INTEGER REFERENCES users, title TEXT, category TEXT);
+            ''')
         return True
 
     def insert_test_user(self):
         cursor = self._connection.cursor()
-        find_user = user_repository.find_user('Testaaja')
+        find_user = self._user_repository.find_user('Testaaja')
         if find_user is False:
             cursor.execute('''
                 INSERT INTO users (username) VALUES ('Testaaja');
                 ''')
             self._connection.commit()
+            cursor.execute('''
+                INSERT INTO test_users (username) VALUES ('Testaaja');
+                ''')
+            self._connection.commit()
+            return True
+        return False
+
+    def drop_tables(self):
+        cursor = self._connection.cursor()
+        cursor.execute('''DROP TABLE IF EXISTS users;''')
+        self._connection.commit()
+        cursor.execute('''DROP TABLE IF EXISTS transactions;''')
+        self._connection.commit()
+        cursor.execute('''DROP TABLE IF EXISTS test_users;''')
+        self._connection.commit()
+        cursor.execute('''DROP TABLE IF EXISTS test_transactions;''')
+        self._connection.commit()
+        return True
 
     def initialize_database(self):
         self.create_tables()
