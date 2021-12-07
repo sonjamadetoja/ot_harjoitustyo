0aa7a312d46da8b8849f65e366d418c6ca19f2be
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2021-12-07 20:58:14 +0200

    update schema with new columns and test tables

--- data/schema.sql
+++ data/schema.sql
@@ -7,6 +7,25 @@ CREATE TABLE users
 CREATE TABLE transactions
 (
 	id INTEGER PRIMARY KEY,
+	date TIMESTAMP,
 	deposit INTEGER,
 	user_id INTEGER REFERENCES users
+	title TEXT, 
+	category TEXT
+);
+
+CREATE TABLE test_users 
+(
+    id INTEGER PRIMARY KEY, 
+    username TEXT UNIQUE
+);
+
+CREATE TABLE test_transactions 
+(
+    id INTEGER PRIMARY KEY, 
+    date TIMESTAMP, 
+    deposits INTEGER, 
+    user_id INTEGER REFERENCES users, 
+    title TEXT, 
+    category TEXT
 );
