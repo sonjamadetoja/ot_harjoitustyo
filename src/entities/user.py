b457c5cbbedd1b8b3ee073006b927aa859f778a6
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2021-12-12 12:24:28 +0200

    add Docstring

--- src/entities/user.py
+++ src/entities/user.py
@@ -1,8 +1,26 @@
 class User:
+    """Tallentaa sisäänkirjautuneen käyttäjän
+    käyttäjätunnuksen ja tietokannan tunnusnumeron.
+
+    Attributes:
+        user_name (string): käyttäjätunnus
+        user_id (integer): tunnusnumero
+    """
 
     def __init__(self, user_name, user_id):
+        """Luokan konstruktori, joka luo uuden käyttäjän.
+
+        Args:
+            user_name (string): käyttäjätunnus
+            user_id (integer): tunnusnumero
+        """
         self.user_name = user_name
         self.user_id = user_id
 
     def get_user_id(self):
+        """Palauttaa kirjautuneena olevan käyttäjän käyttäjätunnuksen.
+
+        Returns:
+            string: Käyttäjätunnus.
+        """
         return self.user_id
