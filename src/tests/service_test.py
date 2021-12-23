77c7f85e7f7600a4faa05281e2cf8cd041b5d188
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2021-12-23 16:58:40 +0200

    remove not-working code

--- src/tests/service_test.py
+++ src/tests/service_test.py
@@ -91,86 +91,6 @@ class TestService(unittest.TestCase):
             response = True
         self.assertEqual(response, True)
 
-    # def test_find_transaction_by_year(self):
-    #     user = self.service.login("Testaaja")
-    #     self.service.add_transaction("2020-11-11", 1500, user, "testi6", "testi")
-    #     self.service.add_transaction("2019-11-11", 1500, user, "testi9", "testi")
-    #     deposits = self.service.find_transaction_by_year(user, "2020")
-    #     print(deposits)
-
-    #     deposits = self.service.find_transactions(user)
-    #     print(deposits)
-
-    #     response = False
-    #     if len(deposits) == 1:
-    #         response = True
-    #     self.assertEqual(response, True)
-    #     # Lisättyjen tapahtumien poistaminen
-    #     first_transaction = deposits[-2]
-    #     second_transaction = deposits[-1]
-    #     id_first = first_transaction[0]
-    #     id_second = second_transaction[0]
-    #     self.service.remove_transaction(id_first)
-    #     self.service.remove_transaction(id_second)
-
-    # def test_find_transaction_by_year_if_none(self):
-    #     user = self.service.login("Testaaja")
-    #     user_id = user.get_user_id()
-    #     self.transaction_repository.remove_all_deposits(user_id)
-    #     self.service.add_transaction("2018-11-11", 1500, user, "testi10", "testi")
-    #     self.service.add_transaction("2019-11-11", 1500, user, "testi11", "testi")
-    #     deposits = self.service.find_transaction_by_year(user, "2019")
-    #     print(deposits)
-    #     response = False
-    #     if not deposits:
-    #         response = True
-    #     user_id = user.get_user_id()
-    #     self.transaction_repository.remove_all_deposits(user_id)
-    #     self.assertEqual(response, False)
-    #     # Lisättyjen tapahtumien poistaminen
-    #     first_transaction = deposits[-2]
-    #     second_transaction = deposits[-1]
-    #     id_first = first_transaction[0]
-    #     id_second = second_transaction[0]
-    #     self.service.remove_transaction(id_first)
-    #     self.service.remove_transaction(id_second)
-
-    # def test_find_transaction_by_month(self):
-    #     user = self.service.login("Testaaja")
-    #     self.service.add_transaction("2020-11-11", 1500, user, "testi12", "testi")
-    #     self.service.add_transaction("2020-12-11", 1500, user, "testi13", "testi")
-    #     self.service.add_transaction("2019-11-11", 1500, user, "testi14", "testi")
-    #     self.service.add_transaction("2019-12-11", 1500, user, "testi15", "testi")
-    #     deposits = self.service.find_transaction_by_month(user, "2020", "11")
-    #     print(deposits)
-
-    #     deposits = self.service.find_transactions(user)
-    #     print(deposits)
-
-    #     response = False
-    #     if len(deposits) == 1:
-    #         response = True
-    #     self.assertEqual(response, True)
-    #     user_id = user.get_user_id()
-    #     self.transaction_repository.remove_all_deposits(user_id)
-
-    # def test_find_transaction_by_month_if_none(self):
-    #     user = self.service.login("Testaaja")
-    #     user_id = user.get_user_id()
-    #     self.transaction_repository.remove_all_deposits(user_id)
-    #     self.service.add_transaction("2018-11-11", 1500, user, "testi16", "testi")
-    #     self.service.add_transaction("2018-12-11", 1500, user, "testi17", "testi")
-    #     self.service.add_transaction("2019-11-11", 1500, user, "testi18", "testi")
-    #     self.service.add_transaction("2019-12-11", 1500, user, "testi19", "testi")
-    #     deposits = self.service.find_transaction_by_month(user, "2019", "11")
-    #     print(deposits)
-    #     response = False
-    #     if not deposits:
-    #         response = True
-    #     user_id = user.get_user_id()
-    #     self.transaction_repository.remove_all_deposits(user_id)
-    #     self.assertEqual(response, False)
-
     def test_remove_transaction(self):
         user = self.service.login("Testaaja")
         self.service.add_transaction("2021-11-11", 1000, user, "testi3a", "testi")
@@ -184,5 +104,3 @@ class TestService(unittest.TestCase):
         id2 = added_transaction[0]
         self.service.remove_transaction(id2)
         self.assertEqual(id2, (id1-1))
-
-    # def test_add_file(self) TEKEMÄTTÄ: miten?
